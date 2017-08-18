e536ae88bd7142190777a8a5dd1ca4e3b152f4df
Skrillcon <guardianx515@gmail.com>
2017-08-18 16:13:25 -0500

    Input and finished setting up GLEW
    Added mouse position, mouse buttons and key presses for input. Finished
    setting up GLEW.

--- Shade-core/Src/graphics/window.h
+++ Shade-core/Src/graphics/window.h
@@ -1,7 +1,11 @@
 #pragma once
 
 #include <iostream>
+#include <GL\glew.h>
 #include <GLFW\glfw3.h>
+
+#define MAX_KEYS	1024	
+#define MAX_BUTTONS	32
 
 namespace shade {
 	namespace graphics {
@@ -11,6 +15,10 @@ namespace shade {
 			int m_Width, m_Height;
 			GLFWwindow *m_Window;
 			bool m_Closed;
+			bool m_Keys[MAX_KEYS];
+			bool m_MouseButtons[MAX_BUTTONS];
+			// Mouse input
+			double mx, my;
 		public:
 			Window(const char *name, int width, int height);
 			~Window();
@@ -20,7 +28,15 @@ namespace shade {
 
 			inline int getWidth() const { return m_Width;   }
 			inline int getHeight() const { return m_Height;	}
+
+			bool isKeyPressed(unsigned int keycode) const;
+			bool isMouseButtonPressed(unsigned int button) const;
+			void getMousePosition(double& x, double& y) const;
 		private:
 			bool init();
+			friend static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+			friend static void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+			friend static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos);
+
 		};
 }	}
--- Shade-core/main.cpp
+++ Shade-core/main.cpp
@@ -7,15 +7,28 @@ int main(int argc, char **argv) {
 	Window window("Shade!", 1280, 720);
 	glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
 	
-	std::cout << glGetString(GL_VERSION) << std::endl;
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
 	
 	while (!window.closed()) {
 		window.clear();
-		glBegin(GL_TRIANGLES);
+		if (window.isMouseButtonPressed(GLFW_MOUSE_BUTTON_LEFT)) {
+			std::cout << "PRESSED" << std::endl;
+		}
+
+		double x, y;
+		window.getMousePosition(x, y);
+		std::cout << x << y << std::endl;
+#if 1
+		glBegin(GL_QUADS);
 		glVertex2f(-.5f, -.5f);
-		glVertex2f(.0f, .5f);
+		glVertex2f(-.5f, .5f);
+		glVertex2f(.5f, .5f);
 		glVertex2f(.5f, -.5f);
 		glEnd();
+#endif
+		glDrawArrays(GL_ARRAY_BUFFER, 0, 6);
 		window.update();
 	}
 
