1aea076a838c81efcd7f7ceb1af750a020ddb19d
Connor Hasting <guardianx515@gmail.com>
2017-08-22 18:21:22 -0500

    Finished setting up shaders. Renders a box with lighting.

--- Shade-core/Src/graphics/shader.h
+++ Shade-core/Src/graphics/shader.h
@@ -3,6 +3,8 @@
 #include <GL\glew.h>
 #include <vector>
 #include <iostream>
+
+#include "..\math\math.h"
 #include "..\utils\fileutils.h"
 
 namespace shade {
@@ -16,10 +18,19 @@ namespace shade {
 			Shader(const char* vertPath, const char* fragPath);
 			~Shader();
 
+
+			void setUniform1f(const GLchar* name, float value);
+			void setUniform1i(const GLchar* name, int value);
+			void setUniform2f(const GLchar* name, const math::vec2& vector);
+			void setUniform3f(const GLchar* name, const math::vec3& vector);
+			void setUniform4f(const GLchar* name, const math::vec4& vector);
+			void setUniformMat4(const GLchar* name, const math::mat4& matrix);
+
 			void enable() const;
 			void disable() const;
 
 		private:
 			GLuint load();
+			GLint getUniformLocation(const GLchar* name);
 		};
 } }
--- Shade-core/main.cpp
+++ Shade-core/main.cpp
@@ -8,32 +8,39 @@ int main(int argc, char **argv) {
 	using namespace math;
 
 	Window window("Shade!", 1280, 720);
-	glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	
-	GLuint vbo;
 
 	GLfloat vertices[] = 
 	{
-		-0.5f, -0.5f, 0.0f,
-		-0.5f,  0.5f, 0.0f,
-		 0.5f,  0.5f, 0.05f,
-		 0.5f,  0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		-0.5f, -0.5f, 0.0f
+		0, 0, 0,
+		8, 0, 0,
+		0, 3, 0,
+		0, 3, 0,
+		8, 3, 0,
+		8, 0, 0
 	};
 
+	GLuint vbo;
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(0);
 
+	mat4 ortho = mat4::orthographic(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f);
+
 	Shader shader("Src/shaders/basic.vert", "Src/shaders/basic.frag");
 	shader.enable();
+	shader.setUniformMat4("pr_matrix", ortho);
+	shader.setUniformMat4("ml_matrix", mat4::translation(vec3(4, 3, 0)));
+	// To make the light in the center you must subtract translation as well
+	shader.setUniform2f("light_pos", vec2(4.0f, 1.5f));
+	shader.setUniform4f("col", vec4(0.0f, 0.8f, 0.2f, 1.0f));
 
 	while (!window.closed()) {
 		window.clear();
-		glDrawArrays(GL_ARRAY_BUFFER, 0, 6);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 		window.update();
 	}
 
