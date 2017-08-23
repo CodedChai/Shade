4959ccb638d4a63dc2735edcbc703aab0ba932c1
Connor Hasting <guardianx515@gmail.com>
2017-08-22 21:00:53 -0500

    Implemented a simple 2D renderer. It takes in a position, size and color.

--- Shade-core/main.cpp
+++ Shade-core/main.cpp
@@ -6,6 +6,9 @@
 #include "Src\graphics\buffers\indexbuffer.h"
 #include "Src\graphics\buffers\vertexarray.h"
 
+#include "Src\graphics\renderer2d.h"
+#include "Src\graphics\simple2drenderer.h"
+
 
 int main(int argc, char **argv) {
 	using namespace shade;
@@ -14,93 +17,32 @@ int main(int argc, char **argv) {
 
 	Window window("Shade!", 1280, 720);
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-	
-#if 0
-	GLfloat vertices[] = 
-	{
-		0, 0, 0,
-		8, 0, 0,
-		0, 3, 0,
-		0, 3, 0,
-		8, 3, 0,
-		8, 0, 0
-	};
 
-	GLuint vbo;
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(0);
-#else 
-	// Clockwise
-	GLfloat vertices[] = {
-		0, 0, 0,
-		0, 3, 0,
-		8, 3, 0,
-		8, 0, 0
-	};
-
-	GLushort indices[] = {
-		0, 1, 2,
-		2, 3, 0
-	};
-	
-	GLfloat colorsA[] = {
-		1, 0, 1, 1,
-		1, 0, 1, 1,
-		1, 0, 1, 1,
-		1, 0, 1, 1
-	};
-	GLfloat colorsB[] = {
-		0.8f, 0.2f, 0.5f, 1,
-		0.8f, 0.2f, 0.5f, 1,
-		0.8f, 0.2f, 0.5f, 1,
-		0.8f, 0.2f, 0.5f, 1
-	};
-	VertexArray sprite1, sprite2;
-	IndexBuffer ibo(indices, 6);
-
-	sprite1.addBuffer(new Buffer(vertices, 4 * 3, 3), 0);
-	sprite1.addBuffer(new Buffer(colorsA, 4 * 4, 4), 1);
-	sprite2.addBuffer(new Buffer(vertices, 4 * 3, 3), 0);
-	sprite2.addBuffer(new Buffer(colorsB, 4 * 4, 4), 1);
-
-#endif 
 
 	mat4 ortho = mat4::orthographic(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f);
+
 
 	Shader shader("Src/shaders/basic.vert", "Src/shaders/basic.frag");
 	shader.enable();
 	shader.setUniformMat4("pr_matrix", ortho);
 	shader.setUniformMat4("ml_matrix", mat4::translate(vec3(4, 3, 0)));
-	// To make the light in the center you must subtract translation as well
-	//shader.setUniform2f("light_pos", vec2(4.0f, 1.5f));
+
+	Renderable2D sprite1(math::vec3(5, 5, 0), math::vec2(4, 4), math::vec4(1, 0, 1, 1), shader);
+	Renderable2D sprite2(math::vec3(7, 1, 0), math::vec2(4, 4), math::vec4(0, 1, 1, 1), shader);
+	Renderable2D sprite3(math::vec3(1, 3, 0), math::vec2(4, 4), math::vec4(1, 1, 0, 1), shader);
+	Simple2DRenderer renderer;
+
 	shader.setUniform4f("col", vec4(0.0f, 0.8f, 0.2f, 1.0f));
 
 	while (!window.closed()) {
 		window.clear();
 		double x, y;
 		window.getMousePosition(x, y);
-		shader.setUniform2f("light_pos", vec2((float)(x * 16.0f / 1280.0f), (float)(9.0f - y * 9.0f / 720.0f)));
-		
-#if 0
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-#else 
-		sprite1.bind();
-		ibo.bind();
-		shader.setUniformMat4("ml_matrix", mat4::translate(vec3(4, 3, 0)));
-		glDrawElements(GL_TRIANGLES, ibo.getCount(), GL_UNSIGNED_SHORT, 0);
-		ibo.unbind();
-		sprite1.unbind();
-
-		sprite2.bind();
-		ibo.bind();
-		shader.setUniformMat4("ml_matrix", mat4::translate(vec3(0, 0, 0)));
-		glDrawElements(GL_TRIANGLES, ibo.getCount(), GL_UNSIGNED_SHORT, 0);
-		ibo.unbind();
-		sprite2.unbind();
-#endif
+		shader.setUniform2f("light_pos", vec2((float)(x * 16.0f / window.getWidth()), (float)(9.0f - y * 9.0f / window.getHeight())));
+		renderer.submit(&sprite1);
+		renderer.submit(&sprite2);
+		renderer.submit(&sprite3);
+		renderer.flush();
 		window.update();
 	}
 
